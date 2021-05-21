715fbe5beeb43ba78bc682cd12b67e22e1b2ecdf
Oscar <oscarlagatta@hotmail.com>
2021-05-21 21:43:55 +0100

    repository pattern implementation

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        // Restful api methods, we are using this methods when we are exposing APIs from 
+        // the client application
+
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetProduct(string id);
+
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string category);
+
+        // CRUD operations
+        Task CreatedProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
